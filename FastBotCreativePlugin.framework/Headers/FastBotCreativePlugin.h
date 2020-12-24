cbb7f61b7714ca3922bff0bbd82e79966e38e0b5
wishes <tangxiaodong@bytedance.com>
2020-12-24 22:11:08 +0800

    aa

---
+++ FastBotCreativePlugin.framework/Headers/FastBotCreativePlugin.h
@@ -0,0 +1,18 @@
+//
+//  FastBotCreativePlugin.h
+//  FastBotCreativePlugin
+//
+//  Created by wishes on 2020/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FastBotCreativePlugin.
+FOUNDATION_EXPORT double FastBotCreativePluginVersionNumber;
+
+//! Project version string for FastBotCreativePlugin.
+FOUNDATION_EXPORT const unsigned char FastBotCreativePluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastBotCreativePlugin/PublicHeader.h>
+
+
